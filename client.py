be0d84afe9713fdfed4108e57c69fc3619719462
Kordell Hutchins <43355784+korryh@users.noreply.github.com>
2020-12-11 18:48:52 -0500

    Add files via upload
    The new server and client with encryption

--- client.py
+++ client.py
@@ -1,38 +1,61 @@
 import socket
 import threading
-
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 class Client:
-    def __init__(self):
+    def __init__(self):  #main method
         self.create_connection()
 
     def create_connection(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        key = bytes("a1b2c3d4a1b2c3d4", "utf_8")
+        iv = bytes("13579bde13579bde", "utf_8")
+        #cipher = AES.new(key, AES.MODE_CBC,iv)
+        cipher= AES.new(key, AES.MODE_CFB, iv)
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Setting up connection
         
-        while 1:
+        while 1: #test server connectivity
             try:
-                host = input('Enter host name --> ')
-                port = int(input('Enter port --> '))
-                self.s.connect((host,port))
+                #host = input('Enter host name --> ')
+                host = "192.168.0.153"
+                print(type(host))
+                port = 4444
+                #port = int(input('Enter port --> '))
+                print(type(port))
+                self.s.connect((host,port)) #Connects to server with port number
                 
                 break
             except:
                 print("Couldn't connect to server")
 
         self.username = input('Enter username --> ')
-        self.s.send(self.username.encode())
+        name = self.username.encode('utf-8')
+        enc_name = cipher.encrypt(name)
+
+        #print(type(self.username.encode()))
+        #length = 16 - (len(name) % 16)
+        #print(length)
+        #name += bytes([length]*length)
+        #print(len(name))
+        print(enc_name)
+        self.s.send(enc_name)# Sends to server
         
         message_handler = threading.Thread(target=self.handle_messages,args=())
         message_handler.start()
 
-        input_handler = threading.Thread(target=self.input_handler,args=())
+        input_handler = threading.Thread(target=self.input_handler,args=(cipher, self.username))
         input_handler.start()
 
-    def handle_messages(self):
-        while 1:
-            print(self.s.recv(1204).decode())
+    def handle_messages(self): #Message itself
 
-    def input_handler(self):
         while 1:
-            self.s.send((self.username+' - '+input()).encode())
+                print(self.s.recv(1024)) #Input length in bytes
+
+    def input_handler(self,cipher, name): #Username
+        while 1:
+            msg = name + ' - ' + input("> ")
+            #print("flag")
+            enc_msg = msg.encode('utf-8')
+            encrypt_msg = cipher.encrypt(enc_msg)
+            self.s.send(( encrypt_msg)) #Username with "-" identifying the message
 
 client = Client()
--- server.py
+++ server.py
@@ -1,48 +1,65 @@
 import socket
 import threading
-
+from Crypto.Cipher import AES
 class Server:
-    def __init__(self):
-        self.start_server()
+    def __init__(self): # Main method
+        self.start_server() #Calls start server method
 
     def start_server(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
-        host = socket.gethostbyname(socket.gethostname())
-        port = int(input('Enter port to run the server on --> '))
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Intializing Socket for connection
+        key = bytes("a1b2c3d4a1b2c3d4", "utf_8")
+        iv = bytes("13579bde13579bde", "utf_8")
+        #cipher = AES.new(key, AES.MODE_CBC, iv)
 
-        self.clients = []
+        host = socket.gethostbyname(socket.gethostname()) #Gets the ip address of the host PC
+        #port = int(input('Enter port to run the server on --> '))
+        port = 4444
 
-        self.s.bind((host,port))
-        self.s.listen(100)
+        self.clients = []#Creates a list of clients, upcapped.
+
+        self.s.bind((host,port)) #uses ip address and port to connect to the server
+        self.s.listen(500) # listen for 500 seconds
     
         print('Running on host: '+str(host))
         print('Running on port: '+str(port))
 
-        self.username_lookup = {}
+        self.username_lookup = {} #Creates a list of usernames to lookup
 
-        while True:
-            c, addr = self.s.accept()
+        while 1:
+            c, addr = self.s.accept() #Waits for connection to accept
+            """
+            c.recv(1024).decode spits errors when vanilla, ignore errors and decode in utf8
+            cipher only works when it is in a 16 byte boundary. Pad it to fit.
+            """
+            #enc_username = bytes(c.recv(1024).decode("utf-8", errors="ignore"), encoding="utf8") #Recieves a username with a limit of 1024 bytes
+            enc_username = c.recv(1024)
 
-            username = c.recv(1024).decode()
-            
+
+            #print(len(enc_username))
+            #length = 16 - (len(enc_username) % 16)
+            #enc_username += bytes([length]*length)
+            dec_cipher = AES.new(key, AES.MODE_CFB, iv)
+            dec_username = dec_cipher.decrypt(enc_username)
+            username = dec_username.decode('utf8')
+            print(username)
             print('New connection. Username: '+str(username))
             self.broadcast('New person joined the room. Username: '+username)
-
+            """
+            Puts the username in a list for username lookup and appends them to clients.
+            """
             self.username_lookup[c] = username
-
             self.clients.append(c)
              
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+            threading.Thread(target=self.handle_client,args=(c,addr,dec_cipher)).start() #Handles clients with threads
 
     def broadcast(self,msg):
         for connection in self.clients:
-            connection.send(msg.encode())
+            connection.send(msg.encode()) # Allows the message to be seen on the server.
 
-    def handle_client(self,c,addr):
-        while True:
+    def handle_client(self,c,addr, dec_cipher):
+        while 1:
             try:
-                msg = c.recv(1024)
+                encode_msg = c.recv(1024) #Recieve message in 1024 bytes
             except:
                 c.shutdown(socket.SHUT_RDWR)
                 self.clients.remove(c)
@@ -51,11 +68,13 @@ class Server:
                 self.broadcast(str(self.username_lookup[c])+' has left the room.')
 
                 break
-
-            if msg.decode() != '':
-                print('New message: '+str(msg.decode()))
+            print(dec_cipher)
+            dec_msg = dec_cipher.decrypt(encode_msg)
+            print(dec_msg)
+            if dec_msg.decode('utf8') != '':
+                print('New message: '+dec_msg.decode('utf-8'))
                 for connection in self.clients:
                     if connection != c:
-                        connection.send(msg)
+                        connection.send(dec_msg)
 
 server = Server()
