55c0126294ccc9c1254c7e8e4b42be0d4d277fe9
Kordell Hutchins <43355784+korryh@users.noreply.github.com>
2020-12-11 23:03:58 -0500

    Add files via upload
    Final Project for MATH314001

--- client.py
+++ client.py
@@ -1,61 +1,93 @@
+"""
+Group 12A
+MATH314 001
+Professor Lauderdale
+12/11/20
+
+"""
 import socket
 import threading
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
 from Crypto.Util.Padding import pad, unpad
 class Client:
     def __init__(self):  #main method
         self.create_connection()
 
     def create_connection(self):
-        key = bytes("a1b2c3d4a1b2c3d4", "utf_8")
-        iv = bytes("13579bde13579bde", "utf_8")
-        #cipher = AES.new(key, AES.MODE_CBC,iv)
-        cipher= AES.new(key, AES.MODE_CFB, iv)
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Setting up connection
-        
+    
+        self.key = RSA.generate(2048)
+        clientPublicKey = self.key.publickey().export_key()
+        self.username = input('Enter username --> ')
+
         while 1: #test server connectivity
             try:
-                host = input('Enter host name --> ')
-                #host = "192.168.0.153"
-                print(type(host))
-                #port = 4444
+                host = input('Enter IP Address --> ')
+
                 port = int(input('Enter port --> '))
-                print(type(port))
                 self.s.connect((host,port)) #Connects to server with port number
                 
                 break
             except:
                 print("Couldn't connect to server")
 
-        self.username = input('Enter username --> ')
-        name = self.username.encode('utf-8')
-        enc_name = cipher.encrypt(name)
+        #Receive the server's public key
+        self.server_public_key = RSA.import_key(self.s.recv(1024))
 
-        #print(type(self.username.encode()))
-        #length = 16 - (len(name) % 16)
-        #print(length)
-        #name += bytes([length]*length)
-        #print(len(name))
-        print(enc_name)
-        self.s.send(enc_name)# Sends to server
+        #Send the client public key to the server
+        self.s.send(clientPublicKey)
+
+        #Receive the server encrypted aes key
+        server_aes_encrypted_key = self.s.recv(1024)
+        rsa_dec_cipher = PKCS1_OAEP.new(self.key)
+        aes_key_message = rsa_dec_cipher.decrypt(server_aes_encrypted_key).decode('utf8').split('\n')
+        aes_key = bytes.fromhex(aes_key_message[0])
+        aes_iv = bytes.fromhex(aes_key_message[1])
+        aes_cipher = AES.new(aes_key, AES.MODE_CFB, aes_iv)
+        decrypt_aes_cipher = AES.new(aes_key, AES.MODE_CFB, aes_iv)
+
+        # Now we send our username to the server
+        name = self.username.encode('utf-8')
+        enc_name = aes_cipher.encrypt(name)
+
+        # Hash and Sign the username message
+        hash_enc_name = SHA256.new(enc_name)
+        name_signature = pss.new(self.key).sign(hash_enc_name)
+        username_message = (enc_name.hex() + '\n' + name_signature.hex()).encode('utf8')
+        self.s.send(username_message) # Sends to server
         
-        message_handler = threading.Thread(target=self.handle_messages,args=())
+        message_handler = threading.Thread(target=self.handle_messages,args=(decrypt_aes_cipher, None))
         message_handler.start()
 
-        input_handler = threading.Thread(target=self.input_handler,args=(cipher, self.username))
+        input_handler = threading.Thread(target=self.input_handler,args=(aes_cipher, self.username))
         input_handler.start()
 
-    def handle_messages(self): #Message itself
-
+    def handle_messages(self, cipher, throwaway): #Message itself
         while 1:
-                print(self.s.recv(1024)) #Input length in bytes
+            msg = self.s.recv(1024).decode('utf8').split('\n')
+            enc_msg = bytes.fromhex(msg[0])
+            sig = bytes.fromhex(msg[1])
+            try:
+                verifier = pss.new(self.server_public_key)
+                message_hash = SHA256.new(enc_msg)
+                verifier.verify(message_hash, sig)
+            except (ValueError, TypeError):
+                print('HMAC FAILED')
+                break
+            dec_msg = cipher.decrypt(enc_msg)
+            print(dec_msg.decode('utf8')) #Input length in bytes
 
     def input_handler(self,cipher, name): #Username
         while 1:
-            msg = name + ' - ' + input("> ")
+            msg = input("> ")
             #print("flag")
             enc_msg = msg.encode('utf-8')
             encrypt_msg = cipher.encrypt(enc_msg)
-            self.s.send(( encrypt_msg)) #Username with "-" identifying the message
+            sig = pss.new(self.key).sign(SHA256.new(encrypt_msg))
+            to_send = (encrypt_msg.hex() + '\n' + sig.hex()).encode('utf8')
+            self.s.send(to_send) #Username with "-" identifying the message
 
 client = Client()
--- server.py
+++ server.py
@@ -1,15 +1,29 @@
+"""
+Group 12A
+MATH314 001
+Professor Lauderdale
+12/11/20
+
+"""
+
 import socket
 import threading
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
 class Server:
     def __init__(self): # Main method
         self.start_server() #Calls start server method
 
     def start_server(self):
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Intializing Socket for connection
-        key = bytes("a1b2c3d4a1b2c3d4", "utf_8")
-        iv = bytes("13579bde13579bde", "utf_8")
-        #cipher = AES.new(key, AES.MODE_CBC, iv)
+
+        self.key = RSA.generate(2048)
+        serverPublicKey = self.key.publickey().export_key()
 
         host = socket.gethostbyname(socket.gethostname()) #Gets the ip address of the host PC
         #port = int(input('Enter port to run the server on --> '))
@@ -31,50 +45,82 @@ class Server:
             c.recv(1024).decode spits errors when vanilla, ignore errors and decode in utf8
             cipher only works when it is in a 16 byte boundary. Pad it to fit.
             """
-            #enc_username = bytes(c.recv(1024).decode("utf-8", errors="ignore"), encoding="utf8") #Recieves a username with a limit of 1024 bytes
-            enc_username = c.recv(1024)
+            # Once a client connects, send them the servers public key
+            c.send(serverPublicKey)
 
+            # The client should then send us the their public key
+            client_public_key = RSA.import_key(c.recv(1024))
 
-            #print(len(enc_username))
-            #length = 16 - (len(enc_username) % 16)
-            #enc_username += bytes([length]*length)
-            dec_cipher = AES.new(key, AES.MODE_CFB, iv)
-            dec_username = dec_cipher.decrypt(enc_username)
-            username = dec_username.decode('utf8')
-            print(username)
-            print('New connection. Username: '+str(username))
-            self.broadcast('New person joined the room. Username: '+username)
+            # Generate an AES key and IV
+            aes_key = get_random_bytes(16)
+            aes_iv = get_random_bytes(16)
+            aes_cipher = AES.new(aes_key, AES.MODE_CFB, aes_iv)
+            encrypt_aes_cipher = AES.new(aes_key, AES.MODE_CFB, aes_iv)
+            # Package the AES key and IV
+            message = aes_key.hex() + '\n' + aes_iv.hex()
+            message_bytes = bytes(message, "utf_8")
+
+            # encryp the message using the clients public key
+            cipher_rsa = PKCS1_OAEP.new(client_public_key)
+            enc_aes_key = cipher_rsa.encrypt(message_bytes)
+
+            # send encrypted aes key to the client
+            c.send(enc_aes_key)
+
+            # recieve the username from the client
+            enc_signed_username_message = c.recv(1024).decode('utf8').split('\n')
+            message = bytes.fromhex(enc_signed_username_message[0])
+            sig = bytes.fromhex(enc_signed_username_message[1])
+            enc_signed_username_hash = SHA256.new(message)
+            verifier = pss.new(client_public_key)
+            try:
+                verifier.verify(enc_signed_username_hash, sig)
+            except (ValueError, TypeError):
+                print('HMAC failed')
+                break
+            
+            dec_username = aes_cipher.decrypt(message).decode('utf8')
             """
             Puts the username in a list for username lookup and appends them to clients.
             """
-            self.username_lookup[c] = username
-            self.clients.append(c)
-             
-            threading.Thread(target=self.handle_client,args=(c,addr,dec_cipher)).start() #Handles clients with threads
+            self.username_lookup[c] = dec_username
+            self.clients.append((c, encrypt_aes_cipher))
+
+            print(f'New connection. Username: {dec_username}')
+            self.broadcast(f'New person joined the room. Username: {dec_username}')
+            
+            threading.Thread(target=self.handle_client,args=(c,aes_cipher,verifier,dec_username)).start() #Handles clients with threads
 
     def broadcast(self,msg):
-        for connection in self.clients:
-            connection.send(msg.encode()) # Allows the message to be seen on the server.
+        for (connection, cipher) in self.clients:
+            enc_msg = cipher.encrypt(msg.encode('utf8'))
+            sig = pss.new(self.key).sign(SHA256.new(enc_msg))
+            to_send = (enc_msg.hex() + '\n' + sig.hex()).encode('utf8')
+            connection.send(to_send) # Allows the message to be seen on the server.
 
-    def handle_client(self,c,addr, dec_cipher):
+    def handle_client(self,c,dec_cipher,verifier,username):
         while 1:
             try:
-                encode_msg = c.recv(1024) #Recieve message in 1024 bytes
+                encode_msg = c.recv(1024).decode('utf8').split('\n') #Recieve message in 1024 bytes
+                enc_message = bytes.fromhex(encode_msg[0])
+                message_sig = bytes.fromhex(encode_msg[1])
             except:
                 c.shutdown(socket.SHUT_RDWR)
                 self.clients.remove(c)
                 
                 print(str(self.username_lookup[c])+' left the room.')
                 self.broadcast(str(self.username_lookup[c])+' has left the room.')
-
                 break
-            print(dec_cipher)
-            dec_msg = dec_cipher.decrypt(encode_msg)
-            print(dec_msg)
-            if dec_msg.decode('utf8') != '':
-                print('New message: '+dec_msg.decode('utf-8'))
-                for connection in self.clients:
-                    if connection != c:
-                        connection.send(dec_msg)
+            message_hash = SHA256.new(enc_message)
+            try:
+                verifier.verify(message_hash, message_sig)
+            except (ValueError, TypeError):
+                print('HMAC failed')
+                break
+            
+            message = dec_cipher.decrypt(enc_message).decode('utf8')
+            to_send = f'{username} - {message}'
+            print(f'New Message: {to_send}')
+            self.broadcast(to_send)
 
 server = Server()
